e206757e1388e09e0f7d2dd6da045768907620df
Paul Konstantin Gerke <pkcakeout@gmail.com>
2021-02-04 18:21:13 +0100

    Finish makerelease
    Signed-off-by: Paul Konstantin Gerke <pkcakeout@gmail.com>

--- makerelease.sh
+++ makerelease.sh
@@ -2,5 +2,10 @@
 
 tag_name="$( niet dockerbase defs.yaml )_munge_$( niet munge_version defs.yaml )_slurm_$( niet slurm_version defs.yaml )"
 
+if ! git diff-index --quiet HEAD -- ; then
+  echo 'Please commit all changes to git before proceeding!'
+  exit 1
+fi
 
-
+git tag "$tag_name"
+git push --tags
