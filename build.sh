a184251ae44b8977553a258dd21f90cb9567965d
Paul Konstantin Gerke <pkcakeout@gmail.com>
2021-02-04 18:00:06 +0100

    Add build shell script
    Signed-off-by: Paul Konstantin Gerke <pkcakeout@gmail.com>

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+docker build \
+  --tag build \
+  --build-arg "munge_version=$( niet munge_version defs.yaml )" \
+  --build-arg "slurm_version=$( niet slurm_version defs.yaml )" \
+  .
+
+mkdir -p export
+
+docker run -t --rm --workdir=/opt -v $PWD/export:/export:rw build \
+  tar cfz /export/munged.tar.gz munge &
+docker run -t --rm --workdir=/opt -v $PWD/export:/export:rw build \
+  tar cfz /export/slurm.tar.gz slurm &
+wait
+docker run -it --rm --workdir=/opt -v $PWD/export:/export:rw build \
+  chmod -R 777 /export/
+
+
