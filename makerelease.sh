0d219b5a6c7ea99604e258809d4905f1cf3d0e05
Paul Konstantin Gerke <pkcakeout@gmail.com>
2021-02-04 18:25:05 +0100

    Fix makerelease.sh
    Signed-off-by: Paul Konstantin Gerke <pkcakeout@gmail.com>

--- makerelease.sh
+++ makerelease.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-tag_name="$( niet dockerbase defs.yaml )_munge_$( niet munge_version defs.yaml )_slurm_$( niet slurm_version defs.yaml )"
+tag_name="`tr -c '[a-zA-Z0-9_\-.]' _  <<< $( niet dockerbase defs.yaml )__munge_$( niet munge_version defs.yaml )__slurm_$( niet slurm_version defs.yaml ) | sed 's/_*$//'`"
+echo "Creating release with tag: $tag_name"
 
 if ! git diff-index --quiet HEAD -- ; then
-  echo 'Please commit all changes to git before proceeding!'
+  echo 'ERROR: Please commit all changes to git before proceeding!'
   exit 1
 fi
 
+set -e
+
+git push
 git tag "$tag_name"
 git push --tags
